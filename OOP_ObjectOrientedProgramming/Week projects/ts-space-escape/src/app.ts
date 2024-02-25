a04a5eeb8d339e0cf19a0da3138fdfb51ddd96a5
popo0015 <popo0015@hz.nl>
2024-02-25 16:38:18 +0100

    Cleaned FRP1 folder and deleted files from OOP

--- OOP_ObjectOrientedProgramming/Week projects/ts-space-escape/src/app.ts
+++
@@ -1,9 +0,0 @@
-import SpaceEscape from './SpaceEscape.js';
-import { GameLoop } from './GameLoop.js';
-
-const game: SpaceEscape = new SpaceEscape(document.getElementById('game') as HTMLCanvasElement);
-
-const gameLoop: GameLoop = new GameLoop(game);
-window.addEventListener('load', () => {
-  gameLoop.start();
-});
--- OOP_ObjectOrientedProgramming/ts-wizards-chest/src/app.ts
+++
@@ -1,7 +0,0 @@
-import WizardsChest from './WizardsChest.js';
-
-const game: WizardsChest = new WizardsChest(document.getElementById('game') as HTMLCanvasElement);
-
-window.addEventListener('load', () => {
-  game.start();
-});
