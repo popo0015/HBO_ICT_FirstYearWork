8d900f85ac34c77335f8ecef736faf0af4085b92
popo0015 <popo0015@hz.nl>
2024-02-25 16:39:44 +0100

    Cleaned OOP folder

--- OOP_ObjectOrientedProgramming/Lecture 7/ts-fruitdrop-day3/src/app.ts
+++
@@ -1,9 +0,0 @@
-import FruitDrop from './FruitDrop.js';
-import { GameLoop } from './GameLoop.js';
-
-const fruitDrop: FruitDrop = new FruitDrop(document.getElementById('game') as HTMLCanvasElement);
-
-const gameLoop: GameLoop = new GameLoop(fruitDrop);
-window.addEventListener('load', () => {
-  gameLoop.start();
-});
